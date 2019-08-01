c43601f55fbfb5579f40465a6976c9695946139c
pivot <18865290907@163.com>
2019-08-01 11:17:17 +0800

    翻译 URL 标示

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Observers\TopicObserver;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Topic;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Topic::observe(new TopicObserver());
     }
 }
